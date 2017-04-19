4eb5c21af9a346781dd1cc8a7147a4bb2fcf5097
xala3pa <salazar3pa@gmail.com>
2017-04-19 22:03:52 +0200

    add stub configuration to work offline (locally)

--- merchant-service/src/test/java/com/xala3pa/services/StockTest.java
+++ merchant-service/src/test/java/com/xala3pa/services/StockTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureStubRunner(ids = "com.xala3pa:store-service:+:stubs:8082", workOffline = true)
 public class StockTest {
 
     @Autowired
